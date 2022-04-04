0c462a00e365678008267a5c613350a6aa14f7dd
Bartalis Albert <bartalisalbert1993@gmail.com>
2022-04-04 22:16:37 +0300

    toate functionalitatile din tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
 // import { TodoItemComponent } from './todo-item/todo-item.component';
-
+import { MatDialogModule} from '@angular/material/dialog';
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
+    EditTodoDialogComponent,
    // TodoItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
 import { DataService } from '../shared/data.service';
 import {Todo} from '../shared/todo.model';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTodoDialogComponent } from '../edit-todo-dialog/edit-todo-dialog.component';
+import tippy from 'tippy.js';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit,AfterViewInit {
 
   todos:Todo[]=[] 
-  // showValidationErros:boolean
-  constructor(private dataservice: DataService) { }
+  @Output() todoClicked: EventEmitter<void> =new EventEmitter()
+  @Output() editClicked: EventEmitter<void> =new EventEmitter()
+  @Output() deleteClicked: EventEmitter<void> =new EventEmitter()
+  showValidationErros:boolean =false
+  constructor(private dataservice: DataService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.todos=this.dataservice.getAllTodos()
@@ -19,12 +26,43 @@ export class TodosComponent implements OnInit {
 onFormSubmit(form:NgForm){
  
   // console.log("FORM Submitted") afiseaza in consola mesaje => ne ajuta la debug
-  // console.log(form)
+  console.log(form)
   if(form.invalid){
-    return alert("form is invalit")
+    return this.showValidationErros=true
   }
   this.dataservice.addTodo(new Todo(form.value.text))
- // this.showValidationErrors=false va functiona cand rezolv eroarea cu validation erros
+  this.showValidationErros=false 
    form.reset()
+   return this.showValidationErros=false
 }
+ngAfterViewInit():void {
+ // tippy()
+}
+onTodoClicked(todo:Todo){
+ // this.todoClicked.emit()
+  todo.completed=!todo.completed
+}
+toggleCompleted(todo:Todo){
+  todo.completed=!todo.completed
+}
+EditTodo(todo:Todo){
+this.dataservice.updateTodo
+const index=this.todos.indexOf(todo)
+let dialogRef = this.dialog.open(EditTodoDialogComponent, {
+
+  width: '600px',
+  data:todo
+});
+dialogRef.afterClosed().subscribe((result)=>{
+   if(result){
+     this.dataservice.updateTodo(index,result)
+   }
+})
+}
+onDeleteClicked(todo:Todo){
+  const index=this.todos.indexOf(todo)
+  this.dataservice.deleteTodo(index)
+
+}
+// 2:35:32
 }
